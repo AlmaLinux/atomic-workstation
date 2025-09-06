69a3464757209f471df4c30f32f72458cb0b6445
Alex Iribarren <alex.iribarren@gmail.com>
2025-09-06 20:29:27 +0200

    Remove ffmpegthumbnailer

--- files/scripts/11-multimedia.sh
+++ files/scripts/11-multimedia.sh
@@ -5,7 +5,6 @@ set -xeuo pipefail
 # Multimedia codecs
 dnf -y install \
     @multimedia \
-    ffmpegthumbnailer \
     libcamera{,-{v4l2,gstreamer,tools}} \
     gstreamer1-plugins-{base,bad-free-libs} \
     lame{,-libs} \
